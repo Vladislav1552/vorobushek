9cba250d7719b47d10c18475e0580896f0d26fa1
Vladislav1552 <vladiskav333@gmail.com>
2019-03-28 12:11:32 +0300

    Updates

---
+++ precache-manifest.9b9a4beedc89e5063ef161edf6aa2f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vorobushek/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "61ef76f556bb4fb581ff",
+    "url": "/vorobushek/static/js/runtime~main.5c41858b.js"
+  },
+  {
+    "revision": "46097eeac49d49db55d6",
+    "url": "/vorobushek/static/js/main.00bfb2ca.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/vorobushek/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "46097eeac49d49db55d6",
+    "url": "/vorobushek/static/css/main.e3f7bbf3.chunk.css"
+  },
+  {
+    "revision": "d5cee0f23c931e87ce836738af94f502",
+    "url": "/vorobushek/index.html"
+  }
+];
